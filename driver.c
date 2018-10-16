cd2142ea06d4d00a54088afd9c7fa975f0baa365
horazio <hfusco@stuy.edu>
2018-10-15 21:25:07 -0400

    added tests then deleted a.out and .o files

--- driver.c
+++ driver.c
@@ -2,23 +2,31 @@
 #include <stdlib.h>
 #include "list.h"
 
+void tester(struct node * n, int num){
+  printf("\n\n--------------- NEW TEST ---------------\n");
+  printf("the list is:\n");
+  print_list(n);
+  printf("\nadding %d to the front\n", num);
+  n = insert_front(n, num);
+  print_list(n);
+}
+
 int main(){
-//struct node * l = null
-//l = insert_front(l, 0)
 
   printf("d\n");
 
   struct node * l = NULL;
+  tester(l, 18);
   l = insert_front(l, 10);
   l = insert_front(l, 8);
   l = insert_front(l, 356);
+  tester(l, 88);
   l = insert_front(l, 123);
   l = insert_front(l, 4);
- 
-  print_list(l);
+  tester(l, 0);
   
   l = free_list(l);
-
-  print_list(l);
+  
+  printf("The list is free because valgrind says so\n");
   return 0;
 }
